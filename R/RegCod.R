041134b61e69efb1b13cc0ea18773b2f3fa2f71e
Likan Zhan <zhanlikan@hotmail.com>
2017-11-18 15:16:19 +0800

    Add a new function to acqr

---
+++ R/RegCod.R
@@ -0,0 +1,30 @@
+RegCod <- 
+function (
+RowLevel = 2, 
+ColLevel = 3, 
+Dummy = TRUE, 
+RowNumber = 1, 
+ColNumber = 1
+) {
+Baseline <- ifelse(Dummy, 0, -1) 
+RowData <- gl(RowLevel, RowNumber)
+ColData <- gl(ColLevel, ColNumber)
+RowLen <- nlevels(RowData)
+ColLen <- nlevels(ColData)
+RowCol <- expand.grid(C = ColData, R = RowData)[, c(2, 1)]
+for (i in 1:(RowLen - 1)){
+RowCol[, paste("R", i, sep = "")] <-
+ifelse(RowCol[, "R"] == RowLen, Baseline,
+ifelse(RowCol[, "R"] == i, 1, 0))
+}
+for (j in 1:(ColLen - 1)){
+RowCol[, paste("C", j,  sep = "")] <-
+ifelse(RowCol[, "C"] == ColLen, Baseline,
+ifelse(RowCol[, "C"] == j, 1, 0))
+}
+for (i in 1:(RowLen - 1)) for (j in 1:(ColLen - 1)){
+RowCol[, paste("R", i, "C", j, sep = "")] <-
+RowCol[, paste("R", i, sep = "")] * RowCol[, paste("C", j, sep = "")]
+}
+return(RowCol)
+}
