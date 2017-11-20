58497bb0b4b110579b9d6687fbbd4a81888ae011
Likan Zhan <zhanlikan@hotmail.com>
2017-11-20 18:04:26 +0800

    Add a new function

--- R/RegCod.R
+++ R/RegCod.R
@@ -11,7 +11,8 @@ RowData <- gl(RowLevel, RowNumber)
 ColData <- gl(ColLevel, ColNumber)
 RowLen <- nlevels(RowData)
 ColLen <- nlevels(ColData)
-RowCol <- expand.grid(C = ColData, R = RowData)[, c(2, 1)]
+RowCol <- expand.grid(C = ColData, R = RowData)
+RowCol <- RowCol[, c(2, 1)]
 for (i in 1:(RowLen - 1)){
 RowCol[, paste("R", i, sep = "")] <-
 ifelse(RowCol[, "R"] == RowLen, Baseline,
@@ -24,9 +25,8 @@ ifelse(RowCol[, "C"] == j, 1, 0))
 }
 for (i in 1:(RowLen - 1)) for (j in 1:(ColLen - 1)){
 RowCol[, paste("R", i, "C", j, sep = "")] <-
-as.numeric(
 RowCol[, paste("R", i, sep = "")] * 
-RowCol[, paste("C", j, sep = "")] )
+RowCol[, paste("C", j, sep = "")]
 }
 return(RowCol)
 }
