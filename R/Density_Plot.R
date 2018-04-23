87b508655e0e89dadc8ee5729fe69ea5d721bbc6
Likan Zhan <zhanlikan@hotmail.com>
2018-04-23 18:31:01 +0800

    Update several functions

--- R/Density_Plot.R
+++ R/Density_Plot.R
@@ -6,20 +6,20 @@ AZ = FALSE, AX = FALSE, arrow = FALSE
 plot(
   X, dnorm(X, mean, sd), xlim = c(min(X), max(X)),
   type = "l", xlab = "", ylab = "", axes = FALSE, col = "#af1e23", lwd = 2, yaxs = "i")
-mtext(bquote(mu==~.(mean)), 1, line = 0.3)
+text(x= mean, y = 0, label = bquote(mu==~.(mean)), adj = c(-0.1, -0.7))
 axis(1, at = c(min(X) - 1.5 * sd, max(X) + 1.5 * sd), labels = c("", ""), line = 0, lwd.ticks = 0)
 if (AX) {
 axis(1, at = seq(min(X) - 1 * sd, max(X) + 1 * sd, by = sd), lwd = 0, line = 0, lwd.ticks = 1)	
 }
 mtext(expression(italic("X")), side = 1, line = -0.5, adj = 1.02)
-if (AZ){
+if (AZ) {
 axis(1, at = c(mean - 3.5 * sd, mean + 3.5 * sd), labels = c("", ""), line = 2.5, lwd.ticks = 0)
 axis(
   1, at = seq(mean - 3 * sd, mean + 3 * sd, by = sd), 
   labels = seq(-3, 3, by = 1), line = 2.5, lwd.ticks = 1)
 mtext(expression(italic("z")), side = 1, line = 2, adj = 1.02)	
 }
-if (arrow){
+if (arrow) {
 segments(
   x0 = seq(mean - 2 * sd, mean + 2 * sd, by = sd), y0 = 0,
   y1 = c(rep(dnorm(mean + sd, mean, sd), 2), dnorm(mean, mean, sd), rep(dnorm(mean + sd, mean, sd), 2)),
@@ -37,5 +37,4 @@ mtext(bquote(X == ~.(point)), side = 1, at = point, line = 2)
 arrows(point, -dnorm(mean + sd, mean, sd) / 6, point, 0, xpd = TRUE, length = 0.08)		
 }
 }
-
 }
--- R/Modified_Frequency_Distribution.R
+++ R/Modified_Frequency_Distribution.R
@@ -1,4 +1,11 @@
-Modified_Frequency_Distribution <- function(data, adjx = 1, ...){
+Modified_Frequency_Distribution <- function(
+data, 
+adjx = 1, 
+mean_sd = FALSE,
+main = "", xlab = "",
+AZ = FALSE,
+...
+){
   Table <- as.data.frame(table(data))
   Table[, "data"] <- as.numeric(as.character(Table[, "data"]))
   ylim <- c(0, max(Table[, "Freq"]) + 1)
@@ -6,11 +13,14 @@ Modified_Frequency_Distribution <- function(data, adjx = 1, ...){
   xlabel <- seq(min(Table[, "data"]), max(Table[, "data"]), by = 1)
   ylabel <- seq(0, max(Table[, "Freq"]), by = 1)
 
-  hist(data, ylim = ylim, xlim = xlim,
-    breaks = seq(min(Table[, "data"]) - 0.5, max(Table[, "data"]) + 0.5, by = 1),
-    yaxs = "i", xaxs = "i", xaxt = "n", yaxt = "n", col = "#fce6bf", border = "#b78135", ...)
+par(mar = c(5, 4, 1, 1.5))
+hist(data, ylim = ylim, xlim = xlim,
+    breaks = seq(min(Table[, "data"]) - 0.5, max(Table[, "data"]) + 0.5, by = 1), 
+    yaxs = "i", xaxs = "i", xaxt = "n", yaxt = "n", col = "#fce6bf", border = "#b78135",
+    main = main, xlab = xlab, ...)
+mtext(expression(italic("X")), side = 1, line = -0.5, adj = 1.02)
 axis(1, at = xlim, labels = c("", ""), lwd.ticks = 0)
-axis(1, at = xlabel, lwd = 0, lwd.ticks = 1)
+axis(1, at = xlabel, labels = xlabel, lwd = 0, lwd.ticks = 1)
 axis(2, at = ylim, labels = c("",""), lwd.ticks = 0)
 axis(2, at = ylabel, lwd = 0, lwd.ticks = 1)
 invisible(lapply(1:nrow(Table), function(i) {
@@ -18,4 +28,18 @@ invisible(lapply(1:nrow(Table), function(i) {
 		segments(Table[i, "data"]  - 0.5, 1:j, Table[i, "data"]  + 0.5, 1:j, col = "#b78135")
 	}))
 }))
+
+if (AZ) {
+axis(1, at = xlim, labels = c("", ""), lwd.ticks = 0, line = 2.5)
+axis(1, at = xlabel, labels = round(scale(xlabel), 2), lwd = 0, lwd.ticks = 1, line = 2.5)
+mtext(expression(italic("z")), side = 1, line = 2, adj = 1.02)	
+}
+
+if (mean_sd) {
+abline(v = mean(data), lty=2)
+abline(v= mean(data) + sd(data), lty = 2)
+text(x = mean(data), y = 0.25, label = bquote(mu==~.(mean(data))), pos = 4)
+arrows(mean(data), ylim[2]-0.5, mean(data) + sd(data), ylim[2] - 0.5, code = 3, length = 0.1)
+text(x = mean(data) + sd(data) / 2, y = ylim[2] - 0.5, label = bquote(sigma==~.(round(sd(data), 2))), pos = 3)	
+}
 }
