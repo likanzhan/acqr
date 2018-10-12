1cfbd79f0c0757f2677bba22194049e0f611d6a4
Likan Zhan <zhanlikan@hotmail.com>
2018-10-12 18:50:20 +0800

    Miles function

---
+++ R/Cassie_Miles.R
@@ -0,0 +1,8 @@
+mls <- function(path = "~/Downloads") {
+    filelist <- list.files(path, pattern = "餐饮专票", recursive = FALSE, full.names = TRUE)
+    datalist <- lapply(filelist, function(x) openxlsx::read.xlsx(x))
+    datafr <- do.call("rbind", datalist)
+    datafr[, "佣金总和"] <- as.numeric(datafr[, "佣金总和"])
+    SMR <- aggregate(datafr["佣金总和"], datafr["商户名（producer）"], sum)
+    readr::write_excel_csv(SMR, "Miles.csv")	
+}
