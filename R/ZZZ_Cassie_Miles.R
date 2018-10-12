d442d1c6ebdc6658f96fec94ec5de6398f06fb72
Likan Zhan <zhanlikan@hotmail.com>
2018-10-12 19:36:12 +0800

    update function

--- R/ZZZ_Cassie_Miles.R
+++ R/ZZZ_Cassie_Miles.R
@@ -1,8 +1,8 @@
-mls <- function(path = "~/Downloads") {
+mls <- function(path = "~/Downloads/mls") {
     filelist <- list.files(path, pattern = "餐饮专票", recursive = FALSE, full.names = TRUE)
     datalist <- lapply(filelist, function(x) openxlsx::read.xlsx(x))
     datafr <- do.call("rbind", datalist)
     datafr[, "佣金总和"] <- as.numeric(datafr[, "佣金总和"])
     SMR <- aggregate(datafr["佣金总和"], datafr["商户名（producer）"], sum)
-    readr::write_excel_csv(SMR, "Miles.csv")	
+    readr::write_excel_csv(SMR, dir.create(path, "Miles.csv"))	
 }
