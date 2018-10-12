2e0a15ab5dc360e671f6df4c4ea7c4757e7b0a1f
Likan Zhan <zhanlikan@hotmail.com>
2018-10-12 22:09:06 +0800

    update mls

--- R/ZZZ_Cassie_Miles.R
+++ R/ZZZ_Cassie_Miles.R
@@ -5,5 +5,5 @@ mls <- function(Directory = "~/Downloads/mls") {
     datafr[, "佣金总和"] <- as.numeric(datafr[, "佣金总和"])
     SMR <- aggregate(datafr["佣金总和"], datafr["商户名（producer）"], sum)
     File_Name <- paste0(abbreviate("Commission"), "_", paste(range(datafr[, "时间周期"]), collapse = "_"), ".csv")
-    readr::write_excel_csv(SMR, file.path(path, File_Name))
+    readr::write_excel_csv(SMR, file.path(Directory, File_Name))
 }
