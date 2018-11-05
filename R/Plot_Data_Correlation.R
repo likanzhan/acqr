10e6c25fc08c528977c61b9fd4ca16a71236c127
Likan Zhan <zhanlikan@hotmail.com>
2018-11-05 10:06:42 +0800

    Update functions

---
+++ R/Plot_Data_Correlation.R
@@ -0,0 +1,32 @@
+Create_Correlation_Data <- function(rr = 1, number = 50, nrm = TRUE){
+if (nrm) {
+  set.seed(2)
+  df <- MASS::mvrnorm(
+    n = number, mu = c(0, 0), 
+    Sigma = matrix(c(1, rr, rr, 1), ncol = 2), 
+    empirical = TRUE)
+    df <- as.data.frame(df)
+    colnames(df) <- c("X", "Y")	
+  } else {	
+  X <- seq(-3, 3, length.out = number)             
+  yfun <- function(x = X, a = 1, h = 0, k = 1)  a * (x - h) ^ 2 + k
+  Y <- yfun(X)
+  df <- data.frame(X = X, Y = Y)	
+ }
+return(df)
+}
+
+Plot_Data_Correlation <- function(rr = 1, number = 50, nrm = TRUE){
+df <- Create_Correlation_Data(rr = rr, number = number, nrm = nrm)
+rr <- cor(df[, "X"], df[, "Y"])
+rr <- round(rr, 3)
+plot(Y ~ X, data = df, pch = 19, col = "#cb2229",
+  bty = "l", xaxt = 'n', yaxt = 'n',
+  xlab = "", ylab = "", font.lab = 2)
+title(xlab = "X", ylab = "Y", line = 0)  
+text(
+  x = mean(range(df[, "X"])),
+  y = max(df[, "Y"]),
+  labels = bquote(r == .(rr))
+  )  
+}
---
+++ R/Plot_Factor_Relation.R
@@ -0,0 +1,42 @@
+#' Plot possible relations between two factors
+#'
+#' Illisturate different possible relations between two factors
+#'
+#' @param R levels of the row factor
+#' @param C levels of the column factor
+#' @param vls values to be draw
+#' @examples
+#' Plot_Factor_Relation()
+
+Plot_Factor_Relation <- function(
+    R = 2, C = 3,
+    vls = 1:(R * C)
+    ) {
+  RowLevel <- paste("R", 1:R, sep = "")
+  ColLevel <- paste("C", 1:C, sep = "")
+  data <- expand.grid(
+    Row = RowLevel, Col = ColLevel, 
+    KEEP.OUT.ATTRS = TRUE, stringsAsFactors = FALSE
+  )
+  data[, "vls"] <- vls
+  pch <- c(1, 19, 21, 0, 15, 22, 5, 18, 23)[1:R]
+  with (data, 
+  interaction.plot(
+      x.factor = Col, 
+      trace.factor = Row, 
+      response = data$vls, cex = 2, lwd = 2, 
+      type = "b", pch = pch, axes = FALSE,
+      ylim = c(0, max(data$vls) + 1),      
+      yaxt = "n", ylab = "", xlab = "Column", 
+      las = 1, xaxs = "i", yaxs = "i"
+    )
+    )
+axis(1, at = c(0, length(unique(data$Col)) + 0.2), 
+    labels = c("", ""), lwd.tick = 0)
+axis(1, at = 1:length(unique(data$Col)), 
+    labels = unique(data$Col), lwd = 0, lwd.ticks = 1)  
+axis(2, at = c(0, max(data$vls) + 1), 
+    labels = c("", ""), lwd.tick = 0)
+mtext(bquote(mu[jk]), line = 0, at = 0.9)
+}
+#Plot_Factor_Relation()
