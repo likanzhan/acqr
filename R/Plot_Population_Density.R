71f615a4ce8127432a56e81992e663a9a106961a
Likan Zhan <zhanlikan@hotmail.com>
2019-03-24 00:18:50 +0800

    Update Functions

--- R/Plot_Population_Density.R
+++ R/Plot_Population_Density.R
@@ -3,8 +3,8 @@
 
 ####### Plot_Population_Density_with_Two_Distributions
 Plot_Population_Density <- function(
-                                    m0 = 0,
-                                    m1 = m0 + 4,
+                                    mean_null = 0,
+                                    mean_alternative = mean_null + 4,
                                     sigma = 1,
                                     sigma_range = 4,
                                     x_range = NULL,
@@ -12,16 +12,21 @@ Plot_Population_Density <- function(
                                     alpha_level = 0.05,
                                     two_tails = TRUE,
                                     show_alpha_level = TRUE,
-                                    null_hypothesis = TRUE, correct_reject = TRUE, false_alarm = TRUE,
-                                    alternative_hypothesis = TRUE, hit = TRUE, miss = TRUE,
+                                    show_hypothesis_null = TRUE, 
+                                    show_correct_reject = TRUE, 
+                                    show_false_alarm = TRUE,
+                                    show_hypothesis_alternative = TRUE, 
+                                    show_hit = TRUE, 
+                                    show_miss = TRUE,
                                     show_sigma = TRUE,
-                                    fill = TRUE,
-                                    decision = TRUE,
-                                    AX = TRUE,
-                                    AZ = FALSE,
+                                    add_fill_color = TRUE,
+                                    show_decision = TRUE,
+                                    show_axis_x = TRUE,
+                                    show_axis_x_label = TRUE,
+                                    show_axis_z = FALSE,
                                     data_points = NULL,
-                                    arrow = FALSE,
-                                    area = NULL) {
+                                    show_arrow = FALSE,
+                                    color_area = NULL) {
   #### Define the colors
   plot_colors <- c("#008744", "#0057e7", "#d62d20", "#ffa700") # google
   # plot_colors <- c("#008744", "#0057e7", "#d62d20", "#ffa700")
@@ -33,19 +38,19 @@ Plot_Population_Density <- function(
 
   #### Calculate critical values
   sigma_m <- sigma / sqrt(n)
-  d <- (m1 - m0) / sigma
+  d <- (mean_alternative - mean_null) / sigma
   ### X and Ys
-  X0s <- seq(m0 - sigma_range * sigma_m, m0 + sigma_range * sigma_m, by = 0.01 * sigma_m)
-  X1s <- seq(m1 - sigma_range * sigma_m, m1 + sigma_range * sigma_m, by = 0.01 * sigma_m)
+  X0s <- seq(mean_null - sigma_range * sigma_m, mean_null + sigma_range * sigma_m, by = 0.01 * sigma_m)
+  X1s <- seq(mean_alternative - sigma_range * sigma_m, mean_alternative + sigma_range * sigma_m, by = 0.01 * sigma_m)
   Xs <- sort(cbind(X0s, X1s))
   if (is.null(x_range)) Xs <- Xs else Xs <- seq(min(x_range), max(x_range), by = 0.01 * sd(x_range))
-  Y0s <- dnorm(Xs, mean = m0, sd = sigma_m)
-  Y1s <- dnorm(Xs, mean = m1, sd = sigma_m)
+  Y0s <- dnorm(Xs, mean = mean_null, sd = sigma_m)
+  Y1s <- dnorm(Xs, mean = mean_alternative, sd = sigma_m)
 
   #### Calculate values for Null Hypothesis
   if (two_tails) {
-    X_min <- qnorm(alpha_level / 2, mean = m0, sd = sigma_m, lower.tail = TRUE)
-    X_max <- qnorm(alpha_level / 2, mean = m0, sd = sigma_m, lower.tail = FALSE)
+    X_min <- qnorm(alpha_level / 2, mean = mean_null, sd = sigma_m, lower.tail = TRUE)
+    X_max <- qnorm(alpha_level / 2, mean = mean_null, sd = sigma_m, lower.tail = FALSE)
     low_tail <- seq(min(min(Xs), X_min), max(min(Xs), X_min), by = sigma_m * 0.001)
     high_tail <- seq(X_max, max(Xs), by = sigma_m * 0.001)
     alpha_value <- sub("^0\\.", ".", formatC(alpha_level / 2, digits = 2, format = "fg"))
@@ -53,42 +58,43 @@ Plot_Population_Density <- function(
     boddy <- seq(X_min, X_max, by = sigma_m * 0.001)
     tail_x <- c(min(low_tail), low_tail, max(low_tail), min(high_tail), high_tail, max(high_tail))
     tail_y <- c(
-      0, dnorm(low_tail, mean = m0, sd = sigma_m), 0,
-      0, dnorm(high_tail, mean = m0, sd = sigma_m), 0
+      0, dnorm(low_tail, mean = mean_null, sd = sigma_m), 0,
+      0, dnorm(high_tail, mean = mean_null, sd = sigma_m), 0
     )
   } else {
-    X_max <- qnorm(alpha_level, mean = m0, sd = sigma_m, lower.tail = FALSE)
+    X_max <- qnorm(alpha_level, mean = mean_null, sd = sigma_m, lower.tail = FALSE)
     high_tail <- seq(X_max, max(Xs), by = sigma_m * 0.001)
     alpha_value <- sub("^0\\.", ".", formatC(alpha_level, digits = 2, format = "fg"))
     correct_rject_value <- sub("^0\\.", ".", formatC(1 - alpha_level, digits = 2, format = "fg"))
     boddy <- seq(min(Xs), X_max, by = sigma_m * 0.001)
     tail_x <- c(min(high_tail), high_tail, max(high_tail))
-    tail_y <- c(0, dnorm(high_tail, mean = m0, sd = sigma_m), 0)
+    tail_y <- c(0, dnorm(high_tail, mean = mean_null, sd = sigma_m), 0)
   }
 
   tail_col <- col2alpha(plot_colors[1], alpha = 0.5)
   body_x <- c(min(boddy), boddy, max(boddy))
-  body_y <- c(0, dnorm(boddy, mean = m0, sd = sigma_m), 0)
+  body_y <- c(0, dnorm(boddy, mean = mean_null, sd = sigma_m), 0)
   body_col <- col2alpha(plot_colors[2], alpha = 0.5)
 
   #### Calculate values for Alternative Hypothesis
-  beta_value <- pnorm(q = high_tail, mean = m1, sd = sigma_m)
+  beta_value <- pnorm(q = high_tail, mean = mean_alternative, sd = sigma_m)
   beta_value <- formatC(beta_value, digits = 2, format = "fg")
   beta_value <- sub("^0\\.", ".", beta_value)
-  power_value <- pnorm(q = high_tail, mean = m1, sd = sigma_m, lower.tail = FALSE)
+  power_value <- pnorm(q = high_tail, mean = mean_alternative, sd = sigma_m, lower.tail = FALSE)
   power_value <- formatC(power_value, digits = 2, format = "f")
   power_value <- sub("^0\\.", ".", power_value)
   beta_range <- seq(min(min(X1s), X_max), max(min(X1s), X_max), by = sigma_m * 0.001)
   beta_x <- c(min(X1s), beta_range, X_max)
-  beta_y <- c(0, dnorm(beta_range, mean = m1, sd = sigma_m), 0)
+  beta_y <- c(0, dnorm(beta_range, mean = mean_alternative, sd = sigma_m), 0)
   beta_col <- col2alpha(plot_colors[3], alpha = 0.5)
   power_range <- seq(X_max, max(X1s), by = sigma_m * 0.001)
   power_x <- c(X_max, power_range, max(X1s))
-  power_y <- c(0, dnorm(power_range, mean = m1, sd = sigma_m), 0)
+  power_y <- c(0, dnorm(power_range, mean = mean_alternative, sd = sigma_m), 0)
   power_col <- col2alpha(plot_colors[4], alpha = 0.5)
 
-  label_pos <- seq(min(Xs), max(Xs), by = sigma_m)
-  label_pos <- unique(c(label_pos, data_points))
+  Xss <- c(Xs, data_points)
+  label_pos <- seq(min(Xss), max(Xss), by = sigma_m)
+  #label_pos <- unique(c(label_pos, data_points))
 
   ######## Do the plot
   plot(NULL, NULL,
@@ -98,86 +104,86 @@ Plot_Population_Density <- function(
   )
 
   ####### Plot the null hypothesis
-  if (null_hypothesis) {
-    if (false_alarm & fill) {
+  if (show_hypothesis_null) {
+    if (show_false_alarm & add_fill_color) {
       polygon(tail_x, tail_y, col = tail_col, border = "white")
       if (show_alpha_level) {
         if (two_tails) {
           text(
-            x = X_min, y = dnorm(m0, m0, sd = sigma_m) / 15,
+            x = X_min, y = dnorm(mean_null, mean_null, sd = sigma_m) / 15,
             label = bquote(alpha / 2 == .(alpha_value)), pos = 2
           )
         } else {
           text(
-            x = X_max, y = dnorm(m0, m0, sd = sigma_m) / 15,
+            x = X_max, y = dnorm(mean_null, mean_null, sd = sigma_m) / 15,
             label = bquote(alpha == .(alpha_value)), pos = 4
           )
         }
       }
     }
-    if (correct_reject & fill) {
+    if (show_correct_reject & add_fill_color) {
       polygon(body_x, body_y, col = body_col, border = "white")
       if (show_alpha_level) {
         text(
-          x = m0 - sigma_m * 0.6, y = dnorm(m0, m0, sd = sigma_m) / 3,
+          x = mean_null - sigma_m * 0.6, y = dnorm(mean_null, mean_null, sd = sigma_m) / 3,
           bquote(1 - alpha == .(correct_rject_value)), pos = 3
         )
       }
     }
     lines(Xs, Y0s, col = "#cf232a", lwd = 2)
     if (show_sigma) {
-      abline(v = m0, lwd = 1)
+      abline(v = mean_null, lwd = 1)
       arrows(
-        x0 = m0, y0 = dnorm(m0 + sigma_m, m0, sigma_m),
-        x1 = m0 + sigma_m, y1 = dnorm(m0 + sigma_m, m0, sigma_m),
+        x0 = mean_null, y0 = dnorm(mean_null + sigma_m, mean_null, sigma_m),
+        x1 = mean_null + sigma_m, y1 = dnorm(mean_null + sigma_m, mean_null, sigma_m),
         length = 0.1
       )
-      if (AX | (!AX & !AZ)) sigma_mm <- sigma_m else sigma_mm <- 1
+      if (show_axis_x | (!show_axis_x & !show_axis_z)) sigma_mm <- sigma_m else sigma_mm <- 1
       text(
-        x = m0 + sigma_m / 2, y = dnorm(m0 + sigma_m, m0, sigma_m), pos = 1,
+        x = mean_null + sigma_m / 2, y = dnorm(mean_null + sigma_m, mean_null, sigma_m), pos = 1,
         labels = if (n == 1) {
           bquote(sigma == .(round(sigma_mm, 2)))
         } else {
           bquote(sigma[M] == .(round(sigma_mm, 2)))
         }
       )
-      mtext(expression(H[0]), side = 3, at = m0)
+      if (show_hypothesis_alternative) mtext(expression(H[0]), side = 3, at = mean_null)
     }
   }
 
   ###### Plot the alternative hypothesis
-  if (alternative_hypothesis) {
-    if (miss & fill) {
+  if (show_hypothesis_alternative) {
+    if (show_miss & add_fill_color) {
       polygon(beta_x, beta_y, col = beta_col, border = "white")
       if (show_alpha_level) {
         text(
-          x = X_max, y = dnorm(m1, m1, sd = sigma_m) / 15,
+          x = X_max, y = dnorm(mean_alternative, mean_alternative, sd = sigma_m) / 15,
           labels = bquote(beta == .(beta_value)), pos = 2
         )
       }
     }
-    if (hit & fill) {
+    if (show_hit & add_fill_color) {
       polygon(power_x, power_y, col = power_col, border = "white")
       if (show_alpha_level) {
         text(
-          x = X_max + abs(m1 - X_max) * 0.5,
-          y = dnorm(m1, m1, sd = sigma_m) / 3,
+          x = X_max + abs(mean_alternative - X_max) * 0.5,
+          y = dnorm(mean_alternative, mean_alternative, sd = sigma_m) / 3,
           labels = bquote(1 - beta == .(power_value)), pos = 4
         )
       }
     }
     lines(Xs, Y1s, col = "#d5493a", lwd = 2)
     if (show_sigma) {
-      abline(v = m1, lwd = 1, col = "gray")
-      mtext(expression(H[1]), side = 3, at = m1)
-      mtext(text = bquote("Cohen's d" == .(d) ~ ", n" == .(n)), side = 3, at = (m0 + m1) / 2, padj = -2)
+      abline(v = mean_alternative, lwd = 1, col = "gray")
+      mtext(expression(H[1]), side = 3, at = mean_alternative)
+      mtext(text = bquote("Cohen's d" == .(d) ~ ", n" == .(n)), side = 3, at = (mean_null + mean_alternative) / 2, padj = -2)
     }
   }
 
   ####### X-Axes and Z-Axes
-  AXZ <- function(Baseline = 2.5, Axis = "z", show_label = TRUE) {
+  show_axis <- function(Baseline = 2.5, Axis = "z", show_label = TRUE) {
     llabel <- if (Axis == "z") {
-      round((label_pos - m0) / sigma_m, 2)
+      round((label_pos - mean_null) / sigma_m, 2)
     } else {
       round(label_pos, 2)
     }
@@ -185,89 +191,98 @@ Plot_Population_Density <- function(
     if (show_label) {
       axis(1, at = label_pos, label = rep("", length(label_pos)), lwd = 0, lwd.ticks = 1, line = Baseline)
       axis(1, at = label_pos, label = llabel, lwd = 0, lwd.ticks = 0, line = Baseline - 0.5)
-      mtext(text = bquote(italic(.(Axis))), side = 1, adj = 1.03, line = Baseline - 0.6)
     }
+      mtext(text = bquote(italic(.(Axis))), side = 1, adj = 1.03, line = Baseline - 0.6)
   }
 
-  if (AX) {
-    if (n == 1) AXZ(0, "X") else AXZ(0, "M")
-    if (AZ) AXZ(2.5, "z")
+  if (show_axis_x) {
+    if (n == 1) show_axis(0, "X", show_label = show_axis_x_label) else show_axis(0, "M", show_label = show_axis_x_label)
+    if (show_axis_z) show_axis(2.5, "z")
   } else {
-    if (AZ) AXZ(0, "z") else AXZ(0, "z", show_label = FALSE)
+    if (show_axis_z) show_axis(0, "z") else show_axis(0, "z", show_label = FALSE)
   }
-
-
   ####### Add an area
-  if (!is.null(area)) {
-    p1 <- area[1]
-    p2 <- area[2]
-    cnvtp <- function(pp) if (pp == Inf) m0 + sigma_range * sigma_m else if (pp == -Inf) m0 - sigma_range * sigma_m else pp
+  if (!is.null(color_area)) {
+    p1 <- color_area[1]
+    p2 <- color_area[2]
+    cnvtp <- function(pp) if (pp == Inf) mean_null + sigma_range * sigma_m else if (pp == -Inf) mean_null - sigma_range * sigma_m else pp
     if (!is.na(p1) & !is.na(p2)) {
       pt1 <- cnvtp(p1)
       pt2 <- cnvtp(p2)
       polygon(c(pt1, pt1, seq(pt1, pt2, length.out = 1000), pt2, pt2),
-        c(0, dnorm(pt1, m0, sigma_m), dnorm(seq(pt1, pt2, length.out = 1000), m0, sigma_m), dnorm(pt2, m0, sigma_m), 0),
+        c(0, dnorm(pt1, mean_null, sigma_m), dnorm(seq(pt1, pt2, length.out = 1000), mean_null, sigma_m), dnorm(pt2, mean_null, sigma_m), 0),
         col = col2alpha("#f4cdba", alpha = 0.8), border = "#af1e23", lwd = 2
       ) ## col2alpha is defined in pPower.R
       percent <- function(x, digits = 2, format = "f", ...) {
         paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
       }
       area_size <- if (p1 == -Inf) {
-        pnorm(p2, m0, sigma_m)
+        pnorm(p2, mean_null, sigma_m)
       } else if (p2 == +Inf) {
-        pnorm(p1, m0, sigma_m, lower.tail = FALSE)
+        pnorm(p1, mean_null, sigma_m, lower.tail = FALSE)
       } else {
-        pnorm(p2, m0, sigma_m) - pnorm(p1, m0, sigma_m)
+        pnorm(p2, mean_null, sigma_m) - pnorm(p1, mean_null, sigma_m)
       }
       area_size <- percent(area_size, 2)
-      text(x = mean(c(pt1, pt2)), y = dnorm(m0 + 2 * sigma_m, m0, sigma_m), label = area_size, pos = 1)
+      text(x = mean(c(pt1, pt2)), y = dnorm(mean_null + 2 * sigma_m, mean_null, sigma_m), label = area_size, pos = 1)
     }
   }
 
   ###### add some data points
   if (!is.null(data_points)) {
     for (point in data_points) {
-      mtext(bquote(X == ~.(point)), side = 1, at = point, line = if (AZ) 4 else 2)
-      arrows(point, -dnorm(m0 + sigma_m, m0, sigma_m) / (if (AZ) 3.8 else 8.5), point, 0, xpd = TRUE, length = 0.08)
+      mtext(bquote(X == ~.(point)), side = 1, at = point, line = if (show_axis_z) 4 else 2, col = "blue")
+      arrows(point, -dnorm(mean_null + sigma_m, mean_null, sigma_m) / (if (show_axis_z) 3.5 else 8.5), point, 0, xpd = TRUE, length = 0.08, col = "blue")
     }
   }
 
   ####### show the decision
-  if (decision) {
+  if (show_decision) {
     abline(v = X_max, lty = 3)
     text(
-      x = X_max, y = dnorm(m0, m0, sd = sigma_m) / 1.1,
+      x = X_max, y = dnorm(mean_null, mean_null, sd = sigma_m) / 1.1,
       label = expression(paste("Reject ", H[0])), pos = 4
     )
     arrows(
-      x0 = X_max, y0 = dnorm(m0, m0, sd = sigma_m) / 1.16,
+      x0 = X_max, y0 = dnorm(mean_null, mean_null, sd = sigma_m) / 1.16,
       x1 = X_max + sigma_m, length = 0.1
     )
     if (two_tails) {
       abline(v = X_min, lty = 3)
       text(
-        x = X_min, y = dnorm(m0, m0, sd = sigma_m) / 1.1,
+        x = X_min, y = dnorm(mean_null, mean_null, sd = sigma_m) / 1.1,
         label = expression(paste("Reject ", H[0])), pos = 2
       )
       arrows(
-        x0 = X_min, y0 = dnorm(m0, m0, sd = sigma_m) / 1.16,
+        x0 = X_min, y0 = dnorm(mean_null, mean_null, sd = sigma_m) / 1.16,
         x1 = X_min - sigma_m, length = 0.1
       )
     }
   }
 
   ####### Add the arrow showing the standard errors
-  if (arrow) {
+  if (show_arrow) {
     segments(
-      x0 = seq(m0 - 2 * sigma_m, m0 + 2 * sigma_m, by = sigma_m), y0 = 0,
+      x0 = seq(mean_null - 2 * sigma_m, mean_null + 2 * sigma_m, by = sigma_m), y0 = 0,
       y1 = c(
-        rep(dnorm(m0 + sigma_m, m0, sigma_m), 2), dnorm(m0, m0, sigma_m),
-        rep(dnorm(m0 + sigma_m, m0, sigma_m), 2)
+        rep(dnorm(mean_null + sigma_m, mean_null, sigma_m), 2), dnorm(mean_null, mean_null, sigma_m),
+        rep(dnorm(mean_null + sigma_m, mean_null, sigma_m), 2)
       ), lty = 5
     )
     arrows(
-      x0 = c(m0, m0, m0 + sigma_m, m0 - sigma_m), y0 = dnorm(m0 + sigma_m, m0, sigma_m),
-      x1 = c(m0 + sigma_m, m0 - sigma_m, m0 + 2 * sigma_m, m0 - 2 * sigma_m), length = 0.08
+      x0 = c(mean_null, mean_null, mean_null + sigma_m, mean_null - sigma_m), 
+      y0 = dnorm(mean_null + sigma_m, mean_null, sigma_m),
+      x1 = c(mean_null + sigma_m, mean_null - sigma_m, mean_null + 2 * sigma_m, mean_null - 2 * sigma_m), length = 0.08
     )
   }
+}
+
+###########################################
+#' Plot density distributions of one population
+#' @export
+Plot_Population_Density_Single <- function(m, s, p = NULL, show_arrow = TRUE, show_decision = FALSE, two_tails = TRUE, alpha_level = 0.05, ...){
+	Plot_Population_Density(
+    mean_null = m, mean_alternative = m, sigma = s, data_point = p, 
+    show_hypothesis_alternative = FALSE, two_tails = two_tails, alpha_level = alpha_level, show_alpha_level = FALSE, 
+    show_decision = show_decision, show_false_alarm = FALSE, show_correct_reject = FALSE, show_arrow = show_arrow, ...)	
 }
