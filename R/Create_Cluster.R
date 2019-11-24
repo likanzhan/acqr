606c3824738117a27f82a18d950944ef6dac74ef
Likan Zhan <zhanlikan@hotmail.com>
2019-11-24 15:57:43 +0800

    Delete a Description

--- R/Create_Cluster.R
+++ R/Create_Cluster.R
@@ -1,9 +1,14 @@
 #' @export
 
 # Source: https://stackoverflow.com/questions/18508363/split-a-numeric-vector-into-continuous-chunks-in-r
+# Source: https://stackoverflow.com/questions/15464793/restoring-original-order-of-a-vector-matrix-in-r
 
 Create_Cluster <- function(vector){
-    Group <- 1 + cumsum(seq_along(vector) %in% (which(diff(vector) > 1) + 1))	
-    ResList  <- split(vector, Group)	
-    return(Group)
+    Original_Order <- order(vector)
+    Sorted_Vector <- vector[Original_Order]
+    Sorted_Group <- 1 + cumsum(seq_along(Sorted_Vector) %in% (which(diff(Sorted_Vector) > 1) + 1))
+    Original_Group <- Sorted_Group[order(Original_Order)]	
+    ResList  <- split(vector, Original_Group)	
+    return(Original_Group)
+	# return(ResList)
 }
